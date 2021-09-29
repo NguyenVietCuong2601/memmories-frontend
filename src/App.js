21ea43bc6fb33f984ce2d90743acb80d7707312d
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-09-29 16:48:29 +0700

    use Redux to get Data, constructing form & create a post

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
+import { getPosts } from "./actions/posts";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 import memories from "./images/memories.png";
-import makeStyles from './styles';
-
+import makeStyles from "./styles";
 
 const App = () => {
     const classes = makeStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, []);
+
     return (
         <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
+            <AppBar
+                className={classes.appBar}
+                position="static"
+                color="inherit"
+            >
+                <Typography
+                    className={classes.heading}
+                    variant="h2"
+                    align="center"
+                >
+                    Memories
+                </Typography>
+                <img
+                    className={classes.image}
+                    src={memories}
+                    alt="memories"
+                    height="60"
+                />
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid
+                        container
+                        justifyContent="space-between"
+                        alignItems="stretch"
+                        spacing={3}
+                    >
                         <Grid item xs={12} sm={7}>
-                            <Posts/>
+                            <Posts />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form/>
+                            <Form />
                         </Grid>
                     </Grid>
                 </Container>
