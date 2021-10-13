133b26d2156cd6ac94b52773df931c89781d573b
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-10-13 23:19:12 +0700

    deploy app

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+    baseURL: "https://memories-project1-mern.herokuapp.com",
+});
 
 // const url = "http://localhost:5000/posts";
 // const url = "https://memories-project1-mern.herokuapp.com/posts";
